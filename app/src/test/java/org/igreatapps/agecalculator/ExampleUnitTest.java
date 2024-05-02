588ff03ea78683d399195a84c33fdeab9a98f478
Vignesh G <107738847+Sweety-Vigneshg@users.noreply.github.com>
2024-05-02 20:32:49 +0530

    Add files via upload

---
+++ app/src/test/java/org/igreatapps/agecalculator/ExampleUnitTest.java
@@ -0,0 +1,12 @@
+package org.igreatapps.agecalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(8, 4 + 4);
+    }
+}
